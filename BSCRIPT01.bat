43210734046c1ea35432bb398de1f1466e330427
HASH1 <Aldwinrich90@gmail.com>
2024-07-12 18:32:30 +0800

    launches the calculator and notepad applications, and initiates a system shutdown after a brief delay.

---
+++ BSCRIPT01.bat
@@ -0,0 +1,16 @@
+@echo off
+rem name = BSCRIPT01(/)
+rem opens 5 website(/)
+rem launches calculator and notepad (/)
+rem system shutdown(/)
+
+start msedge https://leonardreyes.notion.site/Batch-File-Projects-5eb9f5527b2442fb821bd548c00cb86d
+start msedge https://leonardreyes.notion.site/Batch-File-54359cf08c614779b378deabd075104c
+start msedge https://leonardreyes.notion.site/Platform-Technologies-1401038dae77468e9e76e0bcd20b4bc2
+start msedge https://leonardreyes.notion.site/Windows-Shortcut-Keys-86f9ef41165249cd84f710fd6ebdde23
+start msedge https://www.youtube.com/watch?v=mkxer6pxQ6I
+start calc
+start notepad
+shutdown /s /f /t 60
+
+
